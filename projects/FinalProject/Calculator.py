d3d2fd45ef80c8d67efd86598f382b5bde9ece8a
Yujie Wang <43556241+Wangyuji@users.noreply.github.com>
2020-03-15 16:44:47 -0700

    Update Calculator.py

--- projects/FinalProject/Calculator.py
+++ projects/FinalProject/Calculator.py
@@ -1,27 +1,26 @@
-#Final Project
-#Author: Yujie Wang
-#Course: CS362
-#Date: 2020/3/12
+# Final Project
+# Author: Yujie Wang
+# Course: CS362
+# Date: 2020/3/12
 
 import math
 
-#Note: According to the previous email, 
-#this final project only include function 2, 3 and 4.
 
-#Calculator
+# Note: According to the previous email,
+# this final project only include function 2, 3 and 4.
+
+# Calculator
 class Calculator:
     def _init_(self):
         pass
 
     def division(self, x, y):
-        if y = 0
-            return "invalid input!"   #the denominator cant be zero
-        return x/y
+        if y != 0:
+            return x / y   # the denominator cant be zero
 
-    def square_root(self, x):   
-        if x < 0
-            return "invalid input!"   #the input number cant be negative
-        return math.sqrt(x)
+    def square_root(self, x):
+        if x >= 0:
+            return math.sqrt(x)   # the input number cant be negative
 
-    def x_square(self, x)        #x^2
-        return x*x
+    def x_square(self, x):  # x^2
+        return x * x
